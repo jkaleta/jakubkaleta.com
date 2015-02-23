fee7eaf9240336668ecaee1a6e7cfdb7c49c4f0b
Jakub Kaleta <jkaleta@gmail.com>
2015-02-23 21:57:20 +0100

    Initial commit

---
+++ jakubkaleta.com/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jakubkaleta.com.MvcApplication" Language="C#" %>
---
+++ jakubkaleta.com/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jakubkaleta.com.Startup))]
+namespace jakubkaleta.com
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
